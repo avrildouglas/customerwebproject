942b213ae8a678e8f33eea78af4a79bbbaae60c8
avrildouglas <adougla2@montgomerycollege.edu>
2017-06-13 12:01:21 -0400

    second commit

--- src/myFirstServlet.java
+++ src/myFirstServlet.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+public class ProcessingCustomer() {
+    super();
 
 public myFirstServlet() {
     super();
